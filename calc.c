4315ebd44ce13a6e75faaa628129b7ecac817f1a
Cyn8787 <cynthiadiadhiou@gmail.com>
2025-06-14 13:02:51 +0000

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,21 +1,26 @@
 //directive de compilation Alix
+#include <stdio.h>
+float n1,n2;
 
 
 //fonction Addition par Cynthia
 float Addition(float a, float b){
-    return a+b;
+    return a + b;
 }
 
 
 //Fonction soustraction par Fatou
-
+float soustraction(float a,float b){
+    return a - b;
+}
 
 //fonction division par Alix 
-
-
+float division(float a,float b){
+    return a / b;
+}
 //fonction multiplication par Cynthia
 float multiplication(float a,float b){
-    return a*b;
+    return a * b;
 }
 
 
