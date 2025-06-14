ce3f33432f509941fd47a2d019439b4023ef6e99
Cyn8787 <cynthiadiadhiou@gmail.com>
2025-06-14 12:30:03 +0000

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,1 +1,16 @@
-//directive de compilation
+//directive de compilation Alix
+
+
+//fonction Addition par Cynthia
+
+
+//Fonction soustraction par Fatou
+
+
+//fonction division par Alix 
+
+
+//fonction multiplication par Cynthia
+
+
+// methode main par fatou
